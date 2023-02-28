9f077fd4a7b303fb08699405a2473477b3fa4736
Steven J <3893631+AlmostInteractive@users.noreply.github.com>
2023-02-27 18:25:35 -0600

    Missing files

---
+++ Runtime/SerializableKeyValuePair.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace YamaGames.SerializableCollections {
+
+    [Serializable]
+    public struct SerializableKeyValuePair<TKey, TValue> : IKeyValuePairDrawable {
+        public TKey Key;
+        public TValue Value;
+
+        public SerializableKeyValuePair(TKey key, TValue value) {
+            Key = key;
+            Value = value;
+        }
+
+        public void Deconstruct(out TKey key, out TValue value) {
+            key = Key;
+            value = Value;
+        }
+
+        public static implicit operator SerializableKeyValuePair<TKey, TValue>(KeyValuePair<TKey, TValue> kvp) {
+            return new SerializableKeyValuePair<TKey, TValue>(kvp.Key, kvp.Value);
+        }
+
+        public static implicit operator KeyValuePair<TKey, TValue>(SerializableKeyValuePair<TKey, TValue> kvp) {
+            return new KeyValuePair<TKey, TValue>(kvp.Key, kvp.Value);
+        }
+    }
+
+}
